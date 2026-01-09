c525b9be7914689a80580784d07a2eedbc062b78
seongmin2223 <khyuno9@naver.com>
2026-01-09 12:57:59 +0900

    feat: 프로젝트 초기설정 및 디펜던시 추가

---
+++ src/main/java/org/example/resumepro/ResumeProApplication.java
@@ -0,0 +1,13 @@
+package org.example.resumepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/resumepro/ResumeProApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.resumepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
